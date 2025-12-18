daff27a7cf82b0b246156f1b7be17abf8216a504
thinklab-architects <TLA@tlabarc.com>
2025-12-18 15:32:57 +0800

    Import index.css in entry point

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
